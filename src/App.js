c06355a920a7b4ab0417b35327ec26aae2b393d0
cwang76 <92875988+cwang76@users.noreply.github.com>
2025-05-09 23:41:23 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome to Cloudflare Pages</h1>
+      <p>This is a sample React app deployed on Cloudflare.</p>
+    </div>
+  );
+}
+
+export default App;
